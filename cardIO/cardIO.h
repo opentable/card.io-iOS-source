033a244889d0852948c7d778a1f969c00a8f7eb7
Kra Larivain <olarivain@gmail.com>
2015-07-13 06:30:43 -0700

    created project

---
+++ cardIO/cardIO.h
@@ -0,0 +1,19 @@
+//
+//  cardIO.h
+//  cardIO
+//
+//  Created by Olivier Larivain on 6/30/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cardIO.
+FOUNDATION_EXPORT double cardIOVersionNumber;
+
+//! Project version string for cardIO.
+FOUNDATION_EXPORT const unsigned char cardIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cardIO/PublicHeader.h>
+
+
